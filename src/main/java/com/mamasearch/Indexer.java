d342926f36ec0d31a071b493fe3437611a32c7af
Mostafa Hassan <moustafahassan028@gmail.com>
2025-04-24 22:45:58 +0200

    feat: add inverse document frequency calculator

--- src/main/java/com/mamasearch/Indexer.java
+++ src/main/java/com/mamasearch/Indexer.java
@@ -188,5 +188,34 @@ class TFCalculator {
         return tfScores;
     }
 }
+
+
+class IDFCalculator {
+    public static Map<String, Double> calculateIDF(List<List<ParsedWord>> allDocsFilteredWords) {
+        Map<String, Integer> docFreq = new HashMap<>();
+        int totalDocs = allDocsFilteredWords.size();
+
+        for (List<ParsedWord> docWords : allDocsFilteredWords) {
+            Set<String> uniqueWordsInDoc = new HashSet<>();
+            for (ParsedWord pw : docWords) {
+                uniqueWordsInDoc.add(pw.getWord());
+            }
+            for (String word : uniqueWordsInDoc) {
+                docFreq.put(word, docFreq.getOrDefault(word, 0) + 1);
+            }
+        }
+
+        Map<String, Double> idf = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : docFreq.entrySet()) {
+            String word = entry.getKey();
+            int df = entry.getValue();
+            double value = Math.log((double) totalDocs / df);
+            idf.put(word, value);
+        }
+
+        return idf;
+    }
+}
+
 public class Indexer {
 }

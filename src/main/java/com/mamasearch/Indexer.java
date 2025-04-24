b18dd0160a629054fc3bd5eda0a88cf77948429c
Mostafa Hassan <moustafahassan028@gmail.com>
2025-04-24 22:53:27 +0200

    feat: add TF-IDF calculator

--- src/main/java/com/mamasearch/Indexer.java
+++ src/main/java/com/mamasearch/Indexer.java
@@ -217,5 +217,22 @@ class IDFCalculator {
     }
 }
 
+
+class TFIDFScorer {
+    public static Map<String, Double> calculateTFIDF(Map<String, Double> tf, Map<String, Double> idf) {
+        Map<String, Double> tfidf = new HashMap<>();
+
+        for (String word : tf.keySet()) {
+            double tfScore = tf.getOrDefault(word, 0.0);
+            double idfScore = idf.getOrDefault(word, 0.0);  // if IDF missing, assume 0
+            tfidf.put(word, tfScore * idfScore);
+        }
+
+        return tfidf;
+    }
+}
+
+
+
 public class Indexer {
 }

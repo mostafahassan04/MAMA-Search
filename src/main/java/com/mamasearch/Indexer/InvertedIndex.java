5cc7b05ca0831592e3c7a31c53a4fb8e857a8c8d
Mostafa Hassan <moustafahassan028@gmail.com>
2025-04-26 17:19:30 +0300

    feat: add Word Data and Inverted Index

---
+++ src/main/java/com/mamasearch/Indexer/InvertedIndex.java
@@ -0,0 +1,24 @@
+package com.mamasearch.Indexer;
+import java.util.*;
+
+public class InvertedIndex {
+    private Map<String, Map<String, WordData>> index = new HashMap<>();
+
+    public void addWord(String word, String url, int position) {
+        index
+                .computeIfAbsent(word, k -> new HashMap<>())
+                .computeIfAbsent(url, k -> new WordData())
+                .addPosition(position);
+    }
+
+    public void addScore(String word, String url, double score) {
+        if (index.containsKey(word) && index.get(word).containsKey(url)) {
+            index.get(word).get(url).setScore(score);
+        }
+    }
+
+    public Map<String, Map<String, WordData>> getIndex() {
+        return index;
+    }
+}
+
